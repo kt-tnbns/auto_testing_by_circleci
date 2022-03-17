d8fb3ab08a2b33e840c6635bc6a0ab27f42adbcf
flash <Supakorn.prn@gmail.com>
2022-03-18 01:20:54 +0700

    1

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,4 +15,15 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+    public function testUserCreation()
+    {
+    $user = new User([
+        'name' => "Test User",
+        'email' => "test@mail.com",
+        'password' => bcrypt("testpassword")
+    ]);   
+
+    $this->assertEquals('Test User', $user->name);
+    }
+
 }
