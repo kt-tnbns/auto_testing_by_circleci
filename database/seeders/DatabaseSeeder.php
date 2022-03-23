f06ea33d3d42302dc0fbb6a2726e0d014f558627
Tanabut Ngoensri <tangg6@outlook.com>
2022-03-23 10:54:55 +0700

    test\

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();        
     }
 }
