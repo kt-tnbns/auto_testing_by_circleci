7041a9070e45c427ab36a00414a74a461b1c2e41
Tanabut Ngoensri <tangg6@outlook.com>
2022-04-20 23:55:43 +0700

    Edit Task BAT

--- tests/BAT/EditTaskBAT.php
+++ tests/BAT/EditTaskBAT.php
@@ -1,18 +1,26 @@
 <?php
+    
+namespace Tests\BAT;
 
-namespace Tests\Unit;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Tests\TestCase;
 
-use PHPUnit\Framework\TestCase;
-
-class EditTaskBAT extends TestCase
+class EditTaskBAT extends TestCase 
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
+    public function test_edit_task_to_null() {
+        $task = new Task([
+            'user_id' => 1,
+            'description' => 'Test add test to todolist',
+            'date' => '10/10/2022',
+            'deadline' => '11/10/2022'
+        ]);
+
+        $task->description = null;
+
+        $this -> assertEquals(null, $task -> description);
     }
+
 }
