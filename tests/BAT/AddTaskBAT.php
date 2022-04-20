5b61fd95342b2a0166063038e18a7274805671e8
Tanabut Ngoensri <tangg6@outlook.com>
2022-04-20 23:54:20 +0700

    Add Task BAT

--- tests/BAT/AddTaskBAT.php
+++ tests/BAT/AddTaskBAT.php
@@ -1,18 +1,30 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\BAT;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Tests\TestCase;
 
-class AddTaskBAT extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
+class AddTaskBAT extends TestCase{
+    public function test_wrong_deadline(){
+        $task = new Task([
+            'user_id' => '1',
+            'description' => 'learn',
+            'date' => '10/10/2022',
+            'deadline' => '11/9/2022'
+        ]);
+        $this->assertEquals('11/9/2022', $task->deadline);
     }
+
+    public function test_id_detect_null(){
+        $task = new Task([
+            'user_id' => 1,
+            'description' => null,
+        ]);
+        $this->assertEquals(null, $task->description);
+    }
+
 }
